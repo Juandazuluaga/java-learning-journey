56f5204d77d44877b85ad246b10caf10fc65b949
JuandaZuluaga <juandazuluagagiraldo@gmail.com>
2026-02-09 11:28:50 -0500

    Completa ejercicio SentenciasBucleEtiquetas

---
+++ Curso-Udemy/Java/HolaMundo/src/FlujosDeControl/SentenciasBucleEtiquetas.java
@@ -0,0 +1,43 @@
+package FlujosDeControl;
+
+public class SentenciasBucleEtiquetas {
+    static void main(String[] args) {
+
+        bucle:
+        for (int i = 0; i < 5; i++) {
+            int j = 1;
+            while ( j <= 8) {
+                if (i == 6 || i == 7) {
+                    System.out.println("Dia " + i + ": descanso de fin de seman!");
+                    continue bucle;
+                }
+                System.out.println("Dia  " + i + " trabajando a las " + j + " hrs. ");
+                j++;
+            }
+        }
+        System.out.println("\n=======================================================================");
+
+        bucle1:
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+            for (int j = 0; j < 5; j++) {
+                if (i == 2) {
+                    continue bucle1;
+                }
+                System.out.print("[i = " + i + ", j = " + j + "], ");
+            }
+        }
+
+        System.out.println("\n=======================================================================");
+        etiqueta:
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+            for (int j = 0; j < 5; j++) {
+                if (i == 2) {
+                    break etiqueta;
+                }
+                System.out.print("[i = " + i + ", j = " + j + "], ");
+            }
+        }
+    }
+}
